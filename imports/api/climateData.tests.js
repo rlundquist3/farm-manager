b2206e5eb2aa6427c1eb10f010c74460aba0907b
Riley Lundquist <rlundquist3@gmail.com>
2016-10-20 16:07:42 -0700

    added mocha

---
+++ imports/api/climateData.tests.js
@@ -0,0 +1,10 @@
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isServer) {
+  describe('ClimateData', () => {
+    describe('methods', () => {
+      it('stuff & things', () => {
+      });
+    });
+  });
+}
