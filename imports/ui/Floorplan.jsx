d312a932d3f2efd10384b6e93e8e4a4834bfc8f9
Riley Lundquist <rlundquist3@gmail.com>
2016-10-31 14:23:46 -0700

    floorplan updates

--- imports/ui/Floorplan.jsx
+++ imports/ui/Floorplan.jsx
@@ -12,7 +12,10 @@ export default class Floorplan extends Component {
         var width = paper.view.size.width;
         var height = paper.view.size.height;
 
+        paper.view.draw();
+
         var floorplan = {
+          drawing: null,
           size: {
             length: 3*BUILDING_SCALING_FACTOR,
             width: 2*BUILDING_SCALING_FACTOR,
@@ -24,62 +27,71 @@ export default class Floorplan extends Component {
           },
 
           drawOutline: function() {
-            var outline = new paper.Shape.Rectangle({
+            var outline = new paper.Path.Rectangle({
               topLeft: [this.position.x, this.position.y],
               bottomRight: [this.position.x+this.size.width, this.position.y+this.size.length],
               strokeColor: 'black',
             });
+
+            this.drawing = new paper.Group([outline]);
           },
 
           drawRooms: function() {
             var rooms = {
-              incubation: new paper.Shape.Rectangle({
+              incubation: this.drawing.addChild(new paper.Group()),
+              growout1: this.drawing.addChild(new paper.Group()),
+              growout2: this.drawing.addChild(new paper.Group()),
+              breeding: this.drawing.addChild(new paper.Group()),
+            };
+
+            var outlines = {
+              incubation: rooms.incubation.addChild(new paper.Path.Rectangle({
                 topLeft: [this.position.x+this.size.walls, this.position.y+3*this.size.walls+0.55*this.size.length],
                 bottomRight: [this.position.x+this.size.walls+0.45*this.size.width, this.position.y+3*this.size.walls+0.65*this.size.length],
                 strokeColor: 'black',
-              }),
-              growout1: new paper.Shape.Rectangle({
+              })),
+              growout1: rooms.growout1.addChild(new paper.Path.Rectangle({
                 topLeft: [this.position.x+this.size.walls, this.position.y+this.size.walls+0.4*this.size.length],
                 bottomRight: [this.position.x+this.size.walls+0.45*this.size.width, this.position.y+2*this.size.walls+0.55*this.size.length],
                 strokeColor: 'black',
-              }),
-              growout2: new paper.Shape.Rectangle({
+              })),
+              growout2: rooms.growout2.addChild(new paper.Path.Rectangle({
                 topLeft: [this.position.x+this.size.walls, this.position.y+this.size.walls],
                 bottomRight: [this.position.x+this.size.width-this.size.walls, this.position.y+0.4*this.size.length],
                 strokeColor: 'black',
-              }),
-              breeding: new paper.Shape.Rectangle({
+              })),
+              breeding: rooms.breeding.addChild(new paper.Path.Rectangle({
                 topLeft: [this.position.x+0.55*this.size.width, this.position.y+this.size.walls+0.4*this.size.length],
                 bottomRight: [this.position.x+this.size.width-this.size.walls, this.position.y+2*this.size.walls+0.6*this.size.length],
                 strokeColor: 'black',
-              }),
-            };
+              })),
+            }
 
             var text = {
-              incubation: new paper.PointText({
-                point: rooms.incubation.position,
+              incubation: rooms.incubation.addChild(new paper.PointText({
+                point: rooms.incubation.bounds.leftCenter,
                 content: 'Incubation Area',
                 fillColor: 'black',
                 fontSize: '5',
-              }),
-              growout1: new paper.PointText({
-                point: rooms.growout1.position,
+              })),
+              growout1: rooms.growout1.addChild(new paper.PointText({
+                point: rooms.growout1.bounds.leftCenter,
                 content: 'Growout Area 1',
                 fillColor: 'black',
                 fontSize: '5',
-              }),
-              growout2: new paper.PointText({
-                point: rooms.growout2.position,
+              })),
+              growout2: rooms.growout2.addChild(new paper.PointText({
+                point: rooms.growout2.bounds.leftCenter,
                 content: 'Growout Area 2',
                 fillColor: 'black',
                 fontSize: '5',
-              }),
-              breeding: new paper.PointText({
-                point: rooms.breeding.position,
+              })),
+              breeding: rooms.breeding.addChild(new paper.PointText({
+                point: rooms.breeding.bounds.leftCenter,
                 content: 'Breeding Area',
                 fillColor: 'black',
                 fontSize: '5',
-              }),
+              })),
             }
           }
         };
@@ -87,7 +99,7 @@ export default class Floorplan extends Component {
         floorplan.drawOutline();
         floorplan.drawRooms();
 
-        paper.view.draw();
+        floorplan.drawing.position = paper.view.center;
     }
 
     render() {
